d2500a4e56d05cf6660df367c0db7053cd70b44a
AllanLimaS <allan_11_lima@hotmail.com>
2023-09-08 18:10:38 -0300

    novos pngs

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from block import Block
 from game import Game
 import pygame
-import random
 
 pygame.init()
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-
+pygame.display.set_caption("Campo minado - Allan Lima")
 while True:
 
     game = Game(screen)
